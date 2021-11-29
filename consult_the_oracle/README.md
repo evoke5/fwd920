d07887041d2d8a22f5ecf64fda1a9602976d91f5
evoke5 <91923121+evoke5@users.noreply.github.com>
2021-11-28 20:45:43 -0500

    uploaded files

---
+++ consult_the_oracle/README.md
@@ -0,0 +1,1 @@
+# consulttheoracle
---
+++ consult_the_oracle/js/scripts.js
@@ -0,0 +1,133 @@
+// Random number generator
+
+    function getRndInteger() {
+        let random = Math.floor(Math.random() * 12) + 1;
+      console.log(random);
+
+
+// Display card when its corresponding integer is randomly generated
+
+    if (random === 1) {
+        document.getElementById('card1').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+    }
+    else if 
+    (random === 2) {
+        document.getElementById('card2').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+    }
+    else if 
+    (random === 3) {
+        document.getElementById('card3').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+    }
+    else if 
+    (random === 4) {
+        document.getElementById('card4').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+    }
+    else if 
+    (random === 5) {
+        document.getElementById('card5').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+    }
+    else if 
+    (random === 6) {
+        document.getElementById('card6').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+    }
+    else if 
+    (random === 7) {
+        document.getElementById('card7').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+    }
+    else if 
+    (random === 8) {
+        document.getElementById('card8').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+    }
+    else if 
+    (random === 9) {
+        document.getElementById('card9').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+    }
+    else if 
+    (random === 10) {
+        document.getElementById('card10').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+    }
+    else if 
+    (random === 11) {
+        document.getElementById('card11').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+    }
+    else if 
+    (random === 12) {
+        document.getElementById('card12').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+    }
+}
+
+
+// Add toggle functions so user can x out of card and return to homepage to play again
+
+function closeCard1(){
+    document.getElementById('card1').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+}
+
+function closeCard2(){
+    document.getElementById('card2').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+}
+
+function closeCard3(){
+    document.getElementById('card3').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+}
+
+function closeCard4(){
+    document.getElementById('card4').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+}
+
+function closeCard5(){
+    document.getElementById('card5').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+}
+
+function closeCard6(){
+    document.getElementById('card6').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+}
+
+function closeCard7(){
+    document.getElementById('card7').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+}
+
+function closeCard8(){
+    document.getElementById('card8').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+}
+
+function closeCard9(){
+    document.getElementById('card9').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+}
+
+function closeCard10(){
+    document.getElementById('card10').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+}
+
+function closeCard11(){
+    document.getElementById('card11').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+}
+
+function closeCard12(){
+    document.getElementById('card12').classList.toggle('hidden-random');
+        document.getElementById('clickDeck').classList.toggle('hidden-random');
+}
+
